a4c281d842a049210528797eff7be11587898229
mly179 <62368367+mly179@users.noreply.github.com>
2020-03-26 20:26:42 +0700

    Update sinhnhiphan.cpp

--- sinh/sinhnhiphan.cpp
+++ sinh/sinhnhiphan.cpp
@@ -1,63 +1,43 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
-
-
-int  bin[10];
-
-void init(int n){
-    for (int i=0; i<n; i++){
-        bin[i] = 0;
-    }
+int n ,dem=0, a[15];
+void init(){
+	cin >> n;
+	for(int i=1;i<=n;i++){
+		a[i]=0;
+	}
 }
-
-bool check(int n){
-
-    for (int i=0; i<n; i++){
-        if (bin[i] == 0){
-            return 1;
-        }
-    }
-    return 0;
-
+bool isEnd(){
+	for(int i=1;i<=n;i++){
+		if(a[i]==0) return false;
+	}
+	return true;
 }
-
-
-void nextGen(int n){
-
-    int i =  n-1;
-    while(i>=0 && bin[i] != 0) i--;
-
-    bin[i] = 1;
-    for (int j = i+1; j<n; j++){
-        bin[j] = 0;
-    }
+void display(){
+	for(int i=1;i<=n;i++){
+		cout << a[i];
+	
+	}
+	cout << endl;
+	
 }
-
-void display(int n){
-    for (int i=0; i<n; i++){
-        cout<<bin[i];
-    }
-    cout<<endl;
+void gene(){
+	for(int i=n;i>=1;i--){
+		if(a[i]==0){
+			a[i] = 1;
+			for(int j=i+1;j<=n;j++)
+				a[j]=0;
+				break;
+			
+		}
+	}
 }
-
-
-
 int main(){
-    int n;
-    cin>>n;
-    init(n)
-;
-    display(n)
-;
-    while (check(n)){
-        nextGen(n)
-;
-        display(n)
-;
-    }
-
-    cout<<endl;
-
-    return 0;
+	init();
+	while(!isEnd()){
+		display();
+		gene();
+	}
+	display();
+	return 0;
 }
